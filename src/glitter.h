15fbde7710757c46eee62b7aedfd017324786f61
Default User <laurence.a.maddox@gmail.com>
2017-09-10 16:02:23 -0500

    initial commit

---
+++ src/glitter.h
@@ -0,0 +1,16 @@
+#ifndef _GLITTER_H_
+#define _GLITTER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
+#define error_check(C) if (__builtin_expect ((C), false))
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _GLITTER_H_ */
