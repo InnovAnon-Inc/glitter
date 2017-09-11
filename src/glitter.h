5ea2a8c7487e98287766833dbd48795a3d744c74
Default User <laurence.a.maddox@gmail.com>
2017-09-10 19:47:00 -0500

    adding warnings

--- src/glitter.h
+++ src/glitter.h
@@ -7,8 +7,11 @@ extern "C" {
 
 #include <stdbool.h>
 
-#define error_check(C) if    (__builtin_expect ((long) (C), (long) false))
+#define error_check(C)   if    (__builtin_expect ((long) (C), (long) false))
 #define while_check(C,E) while (__builtin_expect ((long) (C), (long) false) && errno == (E))
+
+#define if_expect(C)    if    (__builtin_expect ((long) (C), (long) true))
+#define while_expect(C) while (__builtin_expect ((long) (C), (long) true))
 
 /*#ifdef __cplusplus*/
 #define restrict __restrict__
